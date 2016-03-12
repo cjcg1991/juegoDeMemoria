90d256f030124f534e30b43da828c879157c8894
Christian Javier Carmona <christian.c@christians-macbook-air.local>
2016-03-12 03:11:15 +0200

    Tarea 1.

---
+++ juegoDeMemoria.playground/Contents.swift
@@ -0,0 +1,40 @@
+//: Playground - noun: a place where people can play
+
+
+
+import UIKit
+
+
+var num = 0...100
+
+
+for dib in num {
+    
+    var div = dib % 2
+    var cinco = dib % 5
+    
+    if div == 1 {
+    
+        print("#  \(dib)  Numero Impar" )
+        }else{
+        print("#  \(dib)  Numero Par" )
+    }
+
+    if cinco == 0 {
+        
+        print("#  \(dib)  Bingo" )
+    }
+    
+    
+    if  dib >= 30 && dib <= 40 {
+        
+        print("#  \(dib)  Viva Shift" )
+    }
+
+    
+}
+
+
+
+ 
+
